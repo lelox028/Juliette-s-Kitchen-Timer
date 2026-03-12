bf3785e29cba4e27af95a0bd0ff81e6087536165
Lelox028 <giulianopascarelli@gmail.com>
2026-03-11 21:18:31 -0300

    trying to make it installable

---
+++ juliette-kitchen-timer/public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'juliette-timer-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.jsx',
+  '/src/index.css'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => new Response('Offline'))
+  )
+})
